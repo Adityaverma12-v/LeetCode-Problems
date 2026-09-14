8f8804978819632fc61070d0786819ed62f92aac
Adityaverma2468 <vermaaditya07258@gmail.com>
2026-09-14 22:59:28 +0530

    Time: 4 ms (26.62%) | Memory: 12.4 MB (42.59%) - LeetSync

---
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+ int function( int i, int currentSum, int target, vector<int> & stones, vector<vector<int >> & dp){
+    if(i ==stones .size() ){
+         return currentSum;
+    }
+     if ( dp[i][currentSum] != -1){
+         return dp[i][currentSum];
+     }
+     int exclude = function (i+1, currentSum, target,stones, dp);
+     int include =0;
+     if (currentSum + stones[i] <= target) {
+        include = function ( i+1,  currentSum+stones[i],target,stones, dp);
+     }
+      return  dp[i][currentSum]= max( include , exclude );
+ }
+    int lastStoneWeightII(vector<int>& stones) {
+         int totalSum = accumulate ( stones. begin(), stones.end(), 0);
+         int target= totalSum/ 2;
+         int n= stones .size();
+         vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+         int s1 = function(0, 0, target, stones, dp);
+         return totalSum - 2 * s1;
+    }
+    
+};
